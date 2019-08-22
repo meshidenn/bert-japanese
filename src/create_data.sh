e4ffb55400a0231b52bb0c6aa9fefd4e4f232090
meshidenn <hiroki.iida@retrieva.jp>
2019-08-22 07:29:13 +0000

    first complete multi gpu

--- src/create_data.sh
+++ src/create_data.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-for DIR in $( find /resource/tfbert/asahi/kiji_bert/ -mindepth 1 -type d ); do 
+for DIR in $( find /data/tfbert/wiki/data/ -mindepth 1 -type d ); do 
   python3 src/create_pretraining_data.py \
     --input_file=${DIR}/all.txt \
-    --output_file=${DIR}/all-maxseq128.tfrecord \
-    --model_file=/resource/tfbert/model/asahi.model \
-    --vocab_file=/resource/tfbert/model/asahi.vocab \
+    --output_file=${DIR}/all-maxseq256.tfrecord \
+    --model_file=/data/tfbert/model/wiki-ja.model \
+    --vocab_file=/data/tfbert/model/wiki-ja.vocab \
     --do_lower_case=True \
-    --max_seq_length=128 \
-    --max_predictions_per_seq=20 \
+    --max_seq_length=256 \
+    --max_predictions_per_seq=40 \
     --masked_lm_prob=0.15 \
     --random_seed=12345 \
     --dupe_factor=5 \
